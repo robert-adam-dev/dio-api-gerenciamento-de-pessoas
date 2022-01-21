9f17bec048f291428e7de6d9d53578c2d2523e59
Robert Adam <r.neves1992@gmail.com>
2022-01-21 15:05:01 -0300

    add unit test and readme

---
+++ src/test/java/one/digitalinnovation/dioapigerenciamentodepessoas/utils/PersonUtils.java
@@ -0,0 +1,35 @@
+package one.digitalinnovation.dioapigerenciamentodepessoas.utils;
+
+import one.digitalinnovation.dioapigerenciamentodepessoas.dto.request.PersonDTO;
+import one.digitalinnovation.dioapigerenciamentodepessoas.entity.Person;
+
+import java.util.Collections;
+
+public class PersonUtils {
+    private static final String FIRST_NAME = "Rodrigo";
+    private static final String LAST_NAME = "Peleias";
+    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final long PERSON_ID = 1L;
+    public static final String BIRTH_DATE = "22-09-1992";
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
---
+++ src/test/java/one/digitalinnovation/dioapigerenciamentodepessoas/utils/PhoneUtils.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.dioapigerenciamentodepessoas.utils;
+
+import one.digitalinnovation.dioapigerenciamentodepessoas.dto.request.PhoneDTO;
+import one.digitalinnovation.dioapigerenciamentodepessoas.entity.Phone;
+import one.digitalinnovation.dioapigerenciamentodepessoas.enums.PhoneType;
+
+public class PhoneUtils {
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
